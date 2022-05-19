833e5c6d41ae2cdd70e1940d124a541350d67977
Jurasio54 <103755947+Jurasio54@users.noreply.github.com>
2022-05-18 22:42:57 -0400

    Add files via upload

---
+++ conf.py
@@ -0,0 +1,5 @@
+api_hash = "6817c82fe0b762ed91999f49cd17f7d7"
+api_id = 15472343
+bot_token = "5353495701:AAElYj-mw7cPdNMDSAgkDcMg2hqo9kfARC0"
+authorized_users = [Laamenaza1234,Juanedu_10]
+admin_user = 'demian2008'
