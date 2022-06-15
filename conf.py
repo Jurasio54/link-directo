bb1abf5df32d6fb1bd4773ad6580a542413d4df7
Jurasio54 <103755947+Jurasio54@users.noreply.github.com>
2022-06-15 12:19:48 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,5 +1,5 @@
 api_hash = "6817c82fe0b762ed91999f49cd17f7d7"
 api_id = 15472343
-bot_token = "5256194975:AAF8auH85gGaaDw_NaJ_v3GuJZ7UX3PW-x4"
+bot_token = "5547062260:AAFvH7zWbgAp1m3wooLTO_kB5r_LMpX2AOg"
 authorized_users = [Laamenaza1234]
 admin_user = 'demian2008'
